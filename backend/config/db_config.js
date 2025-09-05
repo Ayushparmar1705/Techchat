5fc6129ba355c4aaf9041ebbb3ce68c327c4f672
Ayush_Parmar <154075430+Ayushparmar1705@users.noreply.github.com>
2025-09-05 17:14:41 +0530

    change the configuration of the database

--- backend/config/db_config.js
+++ backend/config/db_config.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql2");
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Ayush#2004",
-    database: "chatting",
+
+const db = await mysql.createConnection({
+  host: "gateway01-privatelink.ap-southeast-1.prod.aws.tidbcloud.com",
+  port: 4000,
+  user: "23SzzUDureHhtBf.root",
+  password: "ez7Ixuvz71qm8OwT",   // replace with your actual password
+  database: "test",
+  ssl: { rejectUnauthorized: true } // TiDB Cloud requires SSL
 });
 db.connect((err)=>{
     if (err) throw err;
 })
 module.exports = db;
+// mysql://23SzzUDureHhtBf.root:ez7Ixuvz71qm8OwT@gateway01-privatelink.ap-southeast-1.prod.aws.tidbcloud.com:4000/test
